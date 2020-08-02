bddf0aac7e24b109debf9a300e6f962347109bc2
jadhavrk <68983504+jadhavrk@users.noreply.github.com>
2020-08-01 22:14:29 -0500

    Working solution

--- 31-Next-Permutation.py
+++ 31-Next-Permutation.py
@@ -37,9 +37,21 @@ class Solution:
         Failed at [1, 3, 2] -> [2, 1, 3]
         Wrong algo, need to come up with better strategy
         Find the next biggest digit and swap with i - 1 ? Not sure.
+        [1, 3, 2]
+        At i = 1,
+        we need to traverse forward to the right to find the number that is just greater
+        than the element at nums[i-1] and swap with that
         
         
+        
+        Checked solution at [24:00]
+        Basically, only modification would be to change the solution to do a
+        second scan to find a number just larger than element at i - 1
+        Then, sort this array
+        
+        [30:00] Time up. Couldn't code optimal solution.
         '''
+        
         #Edge case
         if not nums:
             return []
@@ -48,10 +60,28 @@ class Solution:
         
         i = len(nums) - 1
         
+        def reverse(nums, st):
+            #sort from starting i
+            i, j = st, len(nums) - 1
+            print(nums)
+            print(i, j)
+            while(i < j):
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+                
+        
         #check from back for dip
         while i > 0:
-            if nums[i] > nums[i - 1]:
-                nums[i], nums[i-1] = nums[i - 1], nums[i]
+            if nums[i] > nums[i - 1]: # Finds first non decreasing number
+                idx_small_swap = i - 1
+                idx_large_swap = len(nums) - 1
+                while idx_large_swap >= 0 and nums[idx_large_swap] <= nums[idx_small_swap]:
+                    idx_large_swap -= 1
+                nums[idx_small_swap], nums[idx_large_swap] = nums[idx_large_swap], nums[idx_small_swap]
+                #reverse in place. 
+                reverse(nums, idx_small_swap + 1)
+                
                 return nums
             i -= 1
         
